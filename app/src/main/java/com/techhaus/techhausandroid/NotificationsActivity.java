8bcdbcbc85923d458787422d5f33233214b4a368
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-06-01 15:13:57 -0300

    Agregada navbar a notifications y routines
    Duda de si tiene q hacerse activity por cada vez q toca navbar o si tiene q ser algo con frame.

--- app/src/main/java/com/techhaus/techhausandroid/NotificationsActivity.java
+++ app/src/main/java/com/techhaus/techhausandroid/NotificationsActivity.java
@@ -1,7 +1,11 @@
 package com.techhaus.techhausandroid;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class NotificationsActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class NotificationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch(item.getItemId()){
+                case R.id.nav_devices:
+                    Intent intent = new Intent(NotificationsActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_notifications:
+                    Intent intent2 = new Intent(NotificationsActivity.this, NotificationsActivity.class);
+                    startActivity(intent2);
+                    break;
+                case R.id.nav_routines:
+                    Intent intent3 = new Intent(NotificationsActivity.this, RoutinesActivity.class);
+                    startActivity(intent3);
+                    break;
+            }
+
+
+            return true;
+        }
+    };
 }
