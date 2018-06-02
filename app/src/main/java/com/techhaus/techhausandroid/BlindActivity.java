d5cb81c827097a7e06315692beb683417d6fac79
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-06-02 18:46:22 -0300

    Diseño de blind
    No se puede acceder desde ningun lado todavia

---
+++ app/src/main/java/com/techhaus/techhausandroid/BlindActivity.java
@@ -0,0 +1,14 @@
+
+package com.techhaus.techhausandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blind);
+    }
+}
