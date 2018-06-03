0d616e8d099aca2a0f3f57cfcb206b7b7b478de7
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-06-03 15:06:16 -0300

    Carga nombres de rutinas desde API
    Falta que funcione el Play
    Falta que se puedan tocar y dar detalles de la rutina

---
+++ app/src/main/java/com/techhaus/techhausandroid/Adapter/MyAdapterRoutines.java
@@ -0,0 +1,55 @@
+package com.techhaus.techhausandroid.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bignerdranch.expandablerecyclerview.Adapter.ExpandableRecyclerAdapter;
+import com.bignerdranch.expandablerecyclerview.Model.ParentObject;
+import com.techhaus.techhausandroid.Models.TitleChild;
+import com.techhaus.techhausandroid.Models.TitleParent;
+import com.techhaus.techhausandroid.R;
+import com.techhaus.techhausandroid.RoutinesActivity;
+import com.techhaus.techhausandroid.ViewHolders.TitleChildViewHolder;
+import com.techhaus.techhausandroid.ViewHolders.TitleParentViewHolder;
+
+import java.util.List;
+
+public class MyAdapterRoutines extends ExpandableRecyclerAdapter<TitleParentViewHolder, TitleChildViewHolder> {
+
+    LayoutInflater inflater;
+
+    public MyAdapterRoutines(Context context, List<ParentObject> parentItemList) {
+        super(context, parentItemList);
+        inflater = LayoutInflater.from(context);
+
+    }
+
+    @Override
+    public TitleParentViewHolder onCreateParentViewHolder(ViewGroup viewGroup) {
+        View view = inflater.inflate(R.layout.list_routines, viewGroup, false);
+        return new TitleParentViewHolder(view);
+    }
+
+    @Override
+    public TitleChildViewHolder onCreateChildViewHolder(ViewGroup viewGroup) {
+        View view = inflater.inflate(R.layout.list_child, viewGroup, false);
+        return new TitleChildViewHolder(view);
+    }
+
+    @Override
+    public void onBindParentViewHolder(TitleParentViewHolder titleParentViewHolder, int i, Object o) {
+        TitleParent title = (TitleParent) o;
+        titleParentViewHolder._textView.setText(title.getTitle());
+    }
+
+    @Override
+    public void onBindChildViewHolder(TitleChildViewHolder titleChildViewHolder, int i, Object o) {
+        TitleChild title = (TitleChild) o;
+        titleChildViewHolder.option1.setText(title.getOption1());
+        titleChildViewHolder.option2.setText(title.getOption2());
+    }
+}
--- app/src/main/java/com/techhaus/techhausandroid/RoutinesActivity.java
+++ app/src/main/java/com/techhaus/techhausandroid/RoutinesActivity.java
@@ -7,14 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bignerdranch.expandablerecyclerview.Model.ParentObject;
 import com.techhaus.techhausandroid.Adapter.MyAdapter;
+import com.techhaus.techhausandroid.Adapter.MyAdapterRoutines;
 import com.techhaus.techhausandroid.Models.TitleChild;
 import com.techhaus.techhausandroid.Models.TitleCreator;
 import com.techhaus.techhausandroid.Models.TitleParent;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +34,9 @@ import java.util.List;
 public class RoutinesActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    private RequestQueue mQueue;
+    List<TitleParent> _titleParents;
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -34,16 +49,28 @@ public class RoutinesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routines);
+        String url = "http://10.0.2.2:8080/api/routines";
+        mQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                processResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("mytag", "Error de response");
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+        Log.d("mytag", "Ya hice el add");
 
         TextView txtInfo = (TextView) findViewById(R.id.txtInfo);
 
 
-        recyclerView = (RecyclerView) findViewById(R.id.myRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        MyAdapter adapter = new MyAdapter(this, initData());
-        adapter.setParentClickableViewAnimationDefaultDuration();
-        adapter.setParentAndIconExpandOnClick(true);
-        recyclerView.setAdapter(adapter);
+
         if(getIntent() != null){
             String info = getIntent().getStringExtra("info");
             txtInfo.setText(info);
@@ -51,6 +78,30 @@ public class RoutinesActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private void processResponse(JSONObject response) {
+        try {
+            Log.d("mytag", "BUENO ACA ESTOY");
+            _titleParents = new ArrayList<>();
+            JSONArray jsonArray = response.getJSONArray("routines");
+            for(int i = 0; i < jsonArray.length(); i++){
+                JSONObject routine = jsonArray.getJSONObject(i);
+                String routineName = routine.getString("name");
+                TitleParent title = new TitleParent(String.format(routineName, i));
+                _titleParents.add(title);
+            }
+            recyclerView = (RecyclerView) findViewById(R.id.myRecyclerView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            MyAdapterRoutines adapter = new MyAdapterRoutines(this, initData());
+            adapter.setParentClickableViewAnimationDefaultDuration();
+            adapter.setParentAndIconExpandOnClick(true);
+            recyclerView.setAdapter(adapter);
+
+        } catch (JSONException e) {
+            Log.d("mytag", "STH WENT WRONG");
+            e.printStackTrace();
+        }
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -80,7 +131,9 @@ public class RoutinesActivity extends AppCompatActivity {
 
     private List<ParentObject> initData() {
         TitleCreator titleCreator = TitleCreator.get(this);
-        List<TitleParent> titles = titleCreator.getAll();
+        Log.d("mytag", "Antes de asignar");
+        List<TitleParent> titles = _titleParents;
+        Log.d("mytag", "Despues de asignar");
         List<ParentObject> parentObject = new ArrayList<>();
         for(TitleParent title: titles){
             List<Object> childList = new ArrayList<>();
