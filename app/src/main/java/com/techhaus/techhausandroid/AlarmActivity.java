58d9493fd403ed237e0e9a676087e176c8d39264
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-06-08 22:28:49 -0300

    Alarma carga estado. Faltan todos los dialogs y ejectura acciones

--- app/src/main/java/com/techhaus/techhausandroid/AlarmActivity.java
+++ app/src/main/java/com/techhaus/techhausandroid/AlarmActivity.java
@@ -7,10 +7,24 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class AlarmActivity extends AppCompatActivity {
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +50,59 @@ public class AlarmActivity extends AppCompatActivity {
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
 
+        mQueue = Volley.newRequestQueue(this);
+        getState(getIntent().getStringExtra("devId"));
 
+
+    }
+
+
+    private void getState(String id) {
+        String url = "http://10.0.2.2:8080/api/devices/" + id + "/getState";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // ok I guess
+
+                JSONArray jsonArray = null;
+                try {
+
+                    JSONObject result = response.getJSONObject("result");
+                    String status = result.getString("status");
+                    TextView what = (TextView) findViewById(R.id.textView2);
+                    what.setText(status);
+                    if(status.equals("armedAway")){
+                        //imagen blind down
+                        //tag de cerrado/down
+                        ImageView icon = (ImageView) findViewById(R.id.alarmStatusIcon);
+                        icon.setImageResource(R.drawable.alarm_wo_people);
+                        icon.setTag(Integer.valueOf(R.drawable.alarm_wo_people));
+                    }else if(status.equals("armedStay")){
+                        //imagen de blind up
+                        //tag de up
+                        ImageView icon = (ImageView) findViewById(R.id.alarmStatusIcon);
+                        icon.setImageResource(R.drawable.alarm_w_people);
+                        icon.setTag(Integer.valueOf(R.drawable.alarm_w_people));
+
+                    }else{
+                        //disarmed
+                        ImageView icon = (ImageView) findViewById(R.id.alarmStatusIcon);
+                        icon.setImageResource(R.drawable.unlocked_inside);
+                        icon.setTag(Integer.valueOf(R.drawable.unlocked_inside));
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("mytag", "Error de response");
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
     }
 
     @Override
