52ff370d911f1501b7deac569cc55b28899e1687
Unknown <mgiorgi@itba.edu.ar>
2018-06-16 19:27:30 -0300

    notif muestra una notif, icono cambiado

--- app/src/main/java/com/techhaus/techhausandroid/NotificationsActivity.java
+++ app/src/main/java/com/techhaus/techhausandroid/NotificationsActivity.java
@@ -1,12 +1,17 @@
 package com.techhaus.techhausandroid;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class NotificationsActivity extends AppCompatActivity {
 
@@ -50,8 +55,34 @@ public class NotificationsActivity extends AppCompatActivity {
                     break;
             }
 
-
             return true;
         }
     };
+
+    public void showNotification(View v) {
+
+        switch(v.getId()) {
+            case R.id.normal_notif:
+                showNormalNotif();
+                break;
+        }
+
+    }
+
+    public void showNormalNotif(){
+
+        //content
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(NotificationsActivity.this);
+        builder.setContentTitle("Security Alert");
+        builder.setContentText("Alarm was turned off");
+        builder.setSmallIcon(R.drawable.alert_icon);
+        builder.setAutoCancel(true);
+        builder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.notif_red));
+
+        //notification manager
+        Notification notification = builder.build();
+        NotificationManager manager = (NotificationManager) this.getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(1234, notification);
+    }
 }
